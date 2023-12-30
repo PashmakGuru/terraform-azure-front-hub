c15073f49795db474bf96b3eee40b92b6a317acb
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2023-12-31 00:25:11 +0330

    Use json file as input

--- example/instance.tf
+++ example/instance.tf
@@ -1,45 +1,19 @@
+locals {
+  data = jsondecode(file("${path.module}/input_processed.json"))
+}
+
 module "front_hub" {
   source  = "./../"
 
   resource_group_name = "front-hub-solution_example-testing"
   resource_group_location = "West US"
 
-  zones = [
-    "pashmak.guru"
-  ]
-  origin_groups = [
-    "argocd-admin-pashmak-guru"
-  ]
-  public_ip_origins = {
-    "argocd-admin-pashmak-guru-kubernetes-cluster" = {
-        origin_group_name = "argocd-admin-pashmak-guru"
-        pip_resource_group_name = "module-azure-administrative-kubernetes-cluster-nodes"
-        pip_name_prefix = "kubernetes-"
-        origin_host_header = "argocd-admin.pashmak.guru"
-    }
-  }
-  endpoints = [
-    "argocd-admin-pashmak-guru"
-  ]
-  rule_sets = [
-    "argocdadminpashmakguru"
-  ]
-  routes = {
-    "argocd-admin-pashmak-guru" = {
-        endpoint_name = "argocd-admin-pashmak-guru",
-        origin_group_name = "argocd-admin-pashmak-guru",
-        origin_names = [
-            "argocd-admin-pashmak-guru-kubernetes-cluster"
-        ],
-        rule_set_names = [
-            "argocdadminpashmakguru"
-        ],
-        patterns_to_match = [
-            "/*"
-        ],
-        use_azure_domain = true
-    }
-  }
+  zones = local.data.zones
+  origin_groups = local.data.origin_groups
+  public_ip_origins = local.data.public_ip_origins
+  endpoints = local.data.endpoints
+  rule_sets = local.data.rule_sets
+  routes = local.data.routes
 }
 
 output "name_servers" {
